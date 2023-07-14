78269f9ed01a5e0abfc2a43969e2971956cb6dd1
Mansoor Khan <thisismansoorkhan@gmail.com>
2023-07-14 18:46:45 +0530

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Mansoor K on 14/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
